e0d32038414e426aae55eff5f75de19666958e2b
Matthew_Verney <matthew.verney@nhs.net>
2024-01-30 16:26:02 +0000

    Added bootstrap to register and login, fix error reporting in login and register page

--- account/forms.py
+++ account/forms.py
@@ -28,7 +28,7 @@ class AccountAuthenticationForm(forms.ModelForm):
 
     def clean(self):
         if self.is_valid():
-            email = self.cleaned_data['email']
-            password = self.cleaned_data['password']
+            email = self.cleaned_data["email"]
+            password = self.cleaned_data["password"]
             if not authenticate(email=email, password=password):
                 raise forms.ValidationError("Invalid login")
