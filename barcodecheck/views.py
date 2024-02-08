7c810ce36a61e0a1663895e5cdedc43c04640d7e
Matthew_Verney <matthew.verney@nhs.net>
2024-02-08 15:59:21 +0000

    updated views.py to simplify getting information from user.
    Bypassing forms.py and passing information from views.py to models.py

--- barcodecheck/views.py
+++ barcodecheck/views.py
@@ -3,26 +3,49 @@ from datetime import datetime
 from .models import BarcodeCheck
 from django.http import HttpResponseRedirect
 from barcodecheck.forms import BarcodeCheck2Form
+from django.contrib import messages
 
 
 def barcodecheck2_view(request):
     context = {}
     if request.method == "POST":
-        form = BarcodeCheck2Form(request.POST)
-        if form.is_valid():
-            BarcodeCheck.barcode1 = form.cleaned_data['barcode1']
-            BarcodeCheck.barcode2 = form.cleaned_data['barcode2']
-            BarcodeCheck.save()
-            # if barcode1 == barcode2
-            #     barcodecheck_result = "MATCH"
-            #     return render(request, 'barcodecheck2')
-
-            # Using home to test is we get to this point... this can be updated later to redirect to new page or return
-            # same page
-            return redirect('home')
+        check = BarcodeCheck()
+        check.worksheet = request.POST['worksheet']
+        check.barcode1 = request.POST['barcode1']
+        check.barcode2 = request.POST['barcode2']
+        check.barcode_check_function = '2 Barcode check'
+        if check.barcode1 == check.barcode2:
+            check.barcodecheck_result = True
+            check.save()
+            return render(request, 'barcodecheck/barcodecheck2.html')
         else:
-            context['barcodecheck2'] = form
+            check.save()
+            return render(request, 'barcodecheck/barcodecheck2.html')
     else:
-        form = BarcodeCheck2Form()
-        context['barcodecheck2_form'] = form
-    return render(request, 'barcodecheck/barcodecheck2.html', context)
+      form = BarcodeCheck2Form()
+      context['barcodecheck2'] = form
+      return render(request, 'barcodecheck/barcodecheck2.html', context)
+
+
+
+    # context = {}
+    # if request.method == "POST":
+    #     form = BarcodeCheck2Form(request.POST)
+    #     if form.is_valid():
+    #         BarcodeCheck.barcode1 = form.cleaned_data['barcode1']
+    #         BarcodeCheck.barcode2 = form.cleaned_data['barcode2']
+    #         BarcodeCheck.save()
+    #         if barcode1 == barcode2
+    #             barcodecheck_result = "MATCH"
+    #             return render(request, 'barcodecheck2')
+    #
+    #         Using home to test is we get to this point... this can be updated later to redirect to new page or return
+    #         same page
+    #         return redirect('home')
+    #     else:
+    #         Need to include an error message here if barcodes do not match (once included above)
+    #         context['barcodecheck2'] = form
+    # else:
+    #     form = BarcodeCheck2Form()
+    #     context['barcodecheck2'] = form
+    #     return render(request, 'barcodecheck/barcodecheck2.html', context)
