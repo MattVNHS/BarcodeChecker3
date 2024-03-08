592abe3168e8ada5e074bfb24711d24ef24a3953
ReubenMonaghan <reuben.monaghan@nhs.net>
2024-03-08 13:14:20 +0000

    homepage barcode_form added

--- homepage/forms.py
+++ homepage/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
+class BarcodeForm(forms.Form):
+    barcode_count = forms.ChoiceField(required=True, label='Barcodes to check',
+                                           choices={i: i for i in range(0,11,2)})
+
 class QiasymphonyForm(forms.Form):
-    barcode_count = forms.ChoiceField(required=True, label='Barcode to check',
-                                           choices=[(8, "8"), (16, "16"),
-                                                   (24, "24"), ("Other", "Other")])
+    barcode_count = forms.ChoiceField(required=True, label='Barcodes to check',
+                                           choices={i: i for i in range(8,25,8)})
+
--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from homepage.forms import *
 def home_screen_view(request):
+    barcode_form = BarcodeForm()
     qiasymphony_form = QiasymphonyForm()
-    context = {'qiasymphony_form': qiasymphony_form}
+    context = {'qiasymphony_form': qiasymphony_form, 'barcode_form': barcode_form}
     if request.method == 'POST':
+        print(request)
         return redirect("Qiasymphony", barcode_count=request.POST['barcode_count'] )
     return render(request, 'homepage/home.html', context)
