ab844117e4c60c297e82077c94a9b964d9f9b437
ReubenMonaghan <reuben.monaghan@nhs.net>
2024-07-11 13:09:10 +0100

    database routers tests added 100% coverage

--- account/views.py
+++ account/views.py
@@ -1,54 +1,35 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-# Might be able to tidy the login view
 
 class Login(TemplateView):
     template_name = 'account/login.html'
     title = 'WMRGL'
 
-    def get(self, request):
-        _context = {
-            "Title": self.title,
-        }
-        return render(request, self.template_name, _context)
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        return data
 
     def post(self, request):
+        _username = request.POST["txtUsername"]
+        _password = request.POST["txtPassword"]
 
-        try:
-            _username = request.POST["txtUsername"]
-            _password = request.POST["txtPassword"]
+        user = authenticate(request, username=_username, password=_password)
 
-            user = authenticate(request, username=_username, password=_password)
+        if user is not None:
 
-            if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('home'))
 
-                login(request, user)
-                return HttpResponseRedirect(reverse('home'))
+        else:
+            # Otherwise, return with failure message
+            messages.warning(self.request, 'The username or password are not valid')
 
-            else:
-                # Otherwise, return with failure message
-
-                _context = {
-                    "Title": self.title,
-                    "error_message": "The username or password are not valid",
-                }
-
-                return render(request, self.template_name, _context)
-
-        except Exception as ex:
-            # Redisplay the login screen
-
-            _context = {
-                "Title": self.title,
-                "error_message": "The username or password are not valid with error message " + str(ex)
-            }
-            print('Exception')
-            return render(request, self.template_name, _context)
-
+            return render(request, self.template_name)
 
 
 # # How to logout of the application
