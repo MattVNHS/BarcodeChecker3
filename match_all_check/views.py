d04d36b4768204a3884de8f9b1057f3718bc9a04
ReubenMonaghan <reuben.monaghan@nhs.net>
2024-08-08 15:12:09 +0100

    match_all_check_audit started

--- match_all_check/views.py
+++ match_all_check/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from match_all_check.forms import *
 from match_all_check.models import *
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -93,7 +94,6 @@ class MatchAllCheckWorksheetView(CreateView):
         context = self.get_context_data()
         barcodes = context['barcodes']
         worksheet_number = context["worksheet_number"]
-        print(worksheet_number)
         self.object = form.save(commit=False)
         self.object.user = self.request.user
         self.object.worksheet, created = Worksheet.objects.get_or_create(worksheet_number=worksheet_number)
@@ -115,3 +115,11 @@ class MatchAllCheckWorksheetView(CreateView):
             return self.form_invalid(form)
         return super().form_valid(form)
 
+class MatchAllCheckListView(ListView):
+    model = MatchAllCheck
+    template_name = 'match_all_check/match_all_check_audit.html'
+    paginate_by = 100  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
