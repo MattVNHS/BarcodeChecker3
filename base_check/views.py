e139722df339d96502ea856009f830798ba2f71a
ReubenMonaghan <reuben.monaghan@nhs.net>
2025-10-09 16:33:41 +0100

    matchallcheck view updated to move more code into base_view

--- base_check/views.py
+++ base_check/views.py
@@ -22,6 +22,25 @@ class CheckView(CreateView):
             data["barcodes"] = get_formset(self.kwargs['barcode_count'], self.model, self.barcode_model, self.barcode_form)
         return data
 
+    def form_valid(self, form):
+        context = self.get_context_data()
+        barcodes = context['barcodes']
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        if barcodes.is_valid():
+            barcodes.instance = self.object
+            barcodes.save()
+            for error in barcodes.errors:
+                messages.warning(self.request, error)
+            if self.object.checkPassFail():
+                messages.success(self.request, 'Check Passed')
+            else:
+                messages.warning(self.request, 'Check Failed - Barcodes do not match')
+        else:
+            return self.form_invalid(form)
+        return super().form_valid(form)
+
     def form_invalid(self, form):
         barcodes = self.get_context_data()['barcodes']
         messages.warning(self.request, form.errors)
--- match_all_check/views.py
+++ match_all_check/views.py
@@ -7,33 +7,12 @@ from audit.views import AuditWorksheetSearchView
 
 # For checks with a worksheet we add 'worksheet' to the fields attribute.
 
-
 @method_decorator(login_required, name='dispatch')
 class MatchAllView(CheckView):
     model = MatchAllCheck
     success_url = '/'
     barcode_model = MatchAllBarcode
     barcode_form = BarcodeForm
-
-    def form_valid(self, form):
-        context = self.get_context_data()
-        barcodes = context['barcodes']
-        self.object = form.save(commit=False)
-        self.object.user = self.request.user
-        self.object.save()
-        if barcodes.is_valid():
-            barcodes.instance = self.object
-            barcodes.save()
-            for error in barcodes.errors:
-                messages.warning(self.request, error)
-            if self.object.checkPassFail():
-                messages.success(self.request, 'Check Passed')
-            else:
-                messages.warning(self.request, 'Check Failed - Barcodes do not match')
-        else:
-            return self.form_invalid(form)
-        return super().form_valid(form)
-
 
 @method_decorator(login_required, name='dispatch')
 class WorksheetMatchAllView(WorksheetCheckView):
