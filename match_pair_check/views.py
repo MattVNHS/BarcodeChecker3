98f420c2704d65fd484d62face1c1ec361a1e990
ReubenMonaghan <reuben.monaghan@nhs.net>
2025-10-13 10:35:13 +0100

    MatchPairValidationMixin added to keep matchpair views DRY

--- match_pair_check/views.py
+++ match_pair_check/views.py
@@ -3,32 +3,33 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from match_pair_check.forms import *
 from audit.views import AuditWorksheetSearchView
+from django.db import transaction
 
 def odd_number_check(barcodes):
-    """Check for odd number of barcodes."""
-    barcodes_entered = [True for x in barcodes if x.has_changed()]
-    if len(barcodes_entered) % 2 != 0:
+    """Check for odd numbers of barcodes."""
+    barcodes_entered_count = sum(1 for form in barcodes if form.has_changed())
+    if barcodes_entered_count % 2 != 0:
         return 'Cannot have an odd number of barcodes entered'
     return None
 
 def assign_comparisonId(barcodes):
     """Assign comparisonId's to barcode instances."""
-    for x in range(len(barcodes)):
-        if x % 2 == 0:
-            barcodes[x].instance.comparisonId = barcodes[x + 1].instance
-        else:
-            barcodes[x].instance.comparisonId = barcodes[x - 1].instance
-    barcodes.save()
+    barcodes = [form for form in barcodes if form.has_changed()]
+    barcode_pairs = zip(barcodes[::2], barcodes[1::2])
+    with transaction.atomic():
+        for even_form, odd_form in barcode_pairs:
+            instance1 = even_form.instance
+            instance2 = odd_form.instance
 
+            instance1.comparisonId = instance2
+            instance2.comparisonId = instance1
 
-@method_decorator(login_required, name='dispatch')
-class WorksheetMatchPairView(WorksheetCheckView):
-    model = MatchPairCheck
-    form_class = MatchPairForm
-    barcode_model = MatchPairBarcode
-    barcode_form = BarcodePairForm
-    success_url = '/'
+            instance1.save()
+            instance2.save()
 
+''' This mixin keeps the worksheet views DRY.
+ Make sure to add it before WorksheetCheckView or AssignedWorksheetCheck or the methods will be overwritten'''
+class MatchPairValidationMixin:
     def validate_barcodes(self, barcodes):
         return odd_number_check(barcodes)
 
@@ -36,20 +37,21 @@ class WorksheetMatchPairView(WorksheetCheckView):
         return assign_comparisonId(barcodes)
 
 @method_decorator(login_required, name='dispatch')
-class AssignedMatchPairView(AssignedWorksheetCheck):
+class WorksheetMatchPairView(MatchPairValidationMixin, WorksheetCheckView):
+    model = MatchPairCheck
+    form_class = MatchPairForm
+    barcode_model = MatchPairBarcode
+    barcode_form = BarcodePairForm
+    success_url = '/'
+
+@method_decorator(login_required, name='dispatch')
+class AssignedMatchPairView(MatchPairValidationMixin, AssignedWorksheetCheck):
     model = MatchPairCheck
     form_class = MatchPairForm
     barcode_model = MatchPairBarcode
     barcode_form = BarcodePairForm
     success_url = '/'
     view_name = 'WorksheetMatchPairView'
-
-    def validate_barcodes(self, barcodes):
-        return odd_number_check(barcodes)
-
-    def process_saved_barcodes(self, barcodes):
-        return assign_comparisonId(barcodes)
-
 
 @method_decorator(login_required, name='dispatch')
 class MatchPairCheckAudit(AuditWorksheetSearchView):
